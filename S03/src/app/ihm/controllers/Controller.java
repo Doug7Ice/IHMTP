2368aa465e0f970ab7e4075a1f275d6765d51bc3
Leo Doug Rey <leo.rey99@gmail.com>
2019-05-03 12:51:25 +0200

    Ajout Slider + mise a jour du temps

--- S03/src/app/ihm/controllers/Controller.java
+++ S03/src/app/ihm/controllers/Controller.java
@@ -3,17 +3,25 @@ package app.ihm.controllers;
 import app.ihm.models.ViewModel;
 import app.workers.Worker;
 import app.workers.WorkerItf;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -39,19 +47,31 @@ public class Controller implements Initializable {
     public MenuItem quitMenu;
     @FXML
     public MenuItem saveMenu;
+    @FXML
+    public Slider sliderTime;
+    @FXML
+    public Label lblTime;
+    @FXML
+    public Label lblCurrentTime;
     private WorkerItf wrk;
     private ViewModel model;
     private MediaPlayer mediaPlayer;
     private FileChooser fileChooser;
+    private boolean isBeingPlayed;
+    private Duration duration;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         wrk = new Worker();
-
         intiTestVideo();
-
         initFileChooser();
-
+        mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+            @Override
+            public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
+                updateValues();
+            }
+        });
+        updateLblTotalDuration();
     }
 
     private void initFileChooser() {
@@ -62,9 +82,41 @@ public class Controller implements Initializable {
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Video Files", "*.mp4"));
     }
 
+    protected void updateValues(){
+        Platform.runLater(new Runnable() {
+            public void run() {
+                Duration currentTime = mediaPlayer.getCurrentTime();
+                String txt = String.format("%.2f",currentTime.toSeconds());
+                lblCurrentTime.setText(txt);
+                sliderTime.setDisable(currentTime.isUnknown());
+                if (!sliderTime.isDisabled() && duration.greaterThan(Duration.ZERO) && !sliderTime.isValueChanging()) {
+                    sliderTime.setValue(currentTime.divide(duration.toMillis()).toMillis() * 100.0);
+                }
+                //if (!volumeSlider.isValueChanging()) {
+                 //   volumeSlider.setValue((int) Math.round(mp.getVolume() * 100));
+               // }
+            }
+        });
+
+    }
+
+    private void updateLblTotalDuration(){
+        mediaPlayer.setOnReady(new Runnable() {
+
+            @Override
+            public void run() {
+                duration = mediaPlayer.getTotalDuration();
+                Double a = duration.toSeconds();
+                String txt = String.format("%.2f",a);
+                lblTime.setText("/"+txt);
+            }
+        });
+    }
+
     private void intiTestVideo() {
 
         try {
+            isBeingPlayed = true;
             File video = new File("src/ressources/test.mp4").getCanonicalFile();
             URI a = video.toURI();
             String b = a.toString();
@@ -87,8 +139,26 @@ public class Controller implements Initializable {
         System.out.println("Heu... Hehe c'est embarrassant.........");
     }
 
+    public void resume(){
+        Duration tempActuel = mediaPlayer.getCurrentTime();
+        Duration tempTotal = mediaPlayer.getTotalDuration();
+        System.out.println();
+        if (isBeingPlayed && !tempActuel.equals(tempTotal)){
+            mediaPlayer.pause();
+            isBeingPlayed = false;
+        }
+        else if (tempActuel.equals(tempTotal)){
+            mediaPlayer.seek(new Duration(0));
+        }
+        else {
+            mediaPlayer.play();
+            isBeingPlayed = true;
+        }
+    }
+
     public void openFile(ActionEvent actionEvent) {
         mediaPlayer.pause();
+        isBeingPlayed = false;
         File selectedFile = fileChooser.showOpenDialog(stage);
         if (selectedFile != null) {
             try {
@@ -97,7 +167,9 @@ public class Controller implements Initializable {
                 Media currentMedia = new Media(selectedFile.toURI().toString());
                 mediaPlayer = new MediaPlayer(currentMedia);
                 mediaView.setMediaPlayer(mediaPlayer);
+                isBeingPlayed = true;
                 mediaPlayer.play();
+                updateLblTotalDuration();
             } catch (Exception e) {
                 System.err.println("ERROR: Unable to open the file");
             }
